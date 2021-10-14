3b6d79a303d0446cf6b27f210c07022d23017bc1
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2021-10-14 23:09:01 +0900

    add error handling to client app.

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+    } catch(err) {
+      setErrors(err.response.data.errors);
+    }
+
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          className="form-control"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        ></input>
+      </div>
+      {errors.length > 0 && <div className="alert alert-danger">
+        <h4>Ooops...</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>}
+      <button className="btn btn-primary">Sign up</button>
+    </form>
+  );
+};
