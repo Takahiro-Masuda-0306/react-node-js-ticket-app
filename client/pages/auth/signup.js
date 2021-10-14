83dbd810d7008770c2670f7d2b7458f3aec94131
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2021-10-15 00:40:45 +0900

    add userRequest Hook. this hook handles user request by url, method type, body.

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,52 +1,46 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import useRequest from "../../hooks/use-request";
 
 export default () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email, password
-      });
-    } catch(err) {
-      setErrors(err.response.data.errors);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
     }
+  });
 
-  }
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    doRequest();
+  };
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
+      <h1>Sign Up</h1>
       <div className="form-group">
-        <label>Email</label>
+        <label>Email Address</label>
         <input
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
+          onChange={e => setEmail(e.target.value)}
           className="form-control"
-        ></input>
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
         <input
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
           type="password"
           className="form-control"
-        ></input>
+        />
       </div>
-      {errors.length > 0 && <div className="alert alert-danger">
-        <h4>Ooops...</h4>
-        <ul className="my-0">
-          {errors.map(err => <li key={err.message}>{err.message}</li>)}
-        </ul>
-      </div>}
-      <button className="btn btn-primary">Sign up</button>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
