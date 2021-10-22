798a73aa957166756f6f275f2998cdcc624250c2
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2021-10-22 11:25:55 +0900

    add nats streaming.

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from './events/tickets-created-listener';
 
 console.clear();
 
@@ -9,33 +10,13 @@ const stan = nats.connect("ticket-app", randomBytes(4).toString("hex"), {
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
-
+  
   stan.on('close', () => {
     console.log('NATS connection closed!');
     process.exit();
   })
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
