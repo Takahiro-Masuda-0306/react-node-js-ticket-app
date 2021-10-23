3f316181521e4fc37a27de81fe4a625f8c13cf75
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2021-10-24 02:41:49 +0900

    complete publishers and listeners.

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -24,6 +24,7 @@ async (req: Request, res: Response) => {
 
   new OrderCancelledPublisher(natsWrapper.client).publish({
     id: order.id,
+    version: order.version,
     ticket: {
       id: order.ticket.id
     }
