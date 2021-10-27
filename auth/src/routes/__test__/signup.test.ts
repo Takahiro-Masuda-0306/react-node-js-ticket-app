dd054dcfa644a0b97dfcaf721907b188eb355b40
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2021-10-28 01:56:50 +0900

    created auth signup/signin/signout test.

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,66 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testtest",
+    })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "tesfafarecom",
+      password: "testtest",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "te",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "testtest",
+    })
+    .expect(400);
+});
+
+it("disallows duplicated email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testtest",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testtest",
+    })
+    .expect(400);
+});
